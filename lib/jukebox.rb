3624872473b12028bfc9bc8bc50e5c7845c48bbe
sam ghoreishi <sam20gh@gmail.com>
2019-02-14 23:09:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,6 +21,10 @@ songs = [
 
 
 def help
-  puts ""
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 end
 
